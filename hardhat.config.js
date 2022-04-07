8c2236506a258347670fa3791f0ad34e617bed91
satibxyz <101393953+satibxyz@users.noreply.github.com>
2022-04-07 09:41:47 +0800

    tasks/deploy-staking-reward: transfer ownership to multisig

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,5 +14,16 @@ module.exports = {
         runs: 200
       }
     }
+  },
+  namedAccounts: {
+    multisig: {
+      hardhat: '0x197939c1ca20C2b506d6811d8B6CDB3394471074',
+      mainnet: '0xA5fC0BbfcD05827ed582869b7254b6f141BA84Eb',
+    },
+  },
+  networks: {
+    ftm: {
+      url: 'https://rpc.ftm.tools',
+    }
   }
 };
